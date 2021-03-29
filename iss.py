8d3e8bc07c84dab747be90460d41b0f5bbc7ad41
Ben Feder <benfeder@Ben.localdomain>
2021-03-29 16:56:07 -0400

    Added three functions, created one of them, but non-functioning

--- iss.py
+++ iss.py
@@ -1,11 +1,45 @@
 #!/usr/bin/env python
 
+import requests
+import time
+
 __author__ = 'Benjamin Feder'
 
 
-def main():
-    pass
+def astronauts_in_space():
+    """Function to obtain list of astronauts currently in space
+    and print the total number in space, their full names, and
+    the spacecraft they are currently on board"""
+    response = requests.get('http://api.open-notify.org/astros.json')
 
+    total_number = response["number"]
+
+    full_names_and_crafts = ""
+    for person in response["people"]:
+        full_names_and_crafts += person["name"] + ": " + person["craft"] + ", "
+
+    print(total_number, full_names_and_crafts)
+
+
+def current_location():
+    """Function obtains current location of ISS and prints current
+    lattitude and longitude along with a timestamp"""
+
+
+def indianapolis_time():
+    """Function to find out next time ISS will be overhead
+    Indianapolis, Indiana and print out the time in
+    human-readable format"""
+
+
+def main():
+    """Run three functions"""
+    astronauts_in_space()
+    current_location()
+    indianapolis_time()
+
+
+main()
 
 if __name__ == '__main__':
     main()
